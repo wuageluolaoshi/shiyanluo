9da540c545ac78f090934556355925bb64d7892f
luopenghui <penghui.luo@wuage.com>
2018-12-19 17:16:51 +0800

    个人税率计算器

--- calculator.py
+++ calculator.py
@@ -2,33 +2,59 @@
 
 import sys
 
+lst = []
+dict1 = {}
 try:
-    salary = int(sys.argv[1])
-    if 3500 <= salary <= 5000:
-        tax = (salary - 3500) * 0.03
-        print('{:.2f}'.format(tax,))
+    before_tax_salarys = sys.argv[1:]
+    for before_tax_salary in before_tax_salarys:
+        lst.append(tuple(before_tax_salary.split(":")))
+    dict1 = dict(lst)
+    for id, before_tax_salary in dict1.items():
+        take_tax = int(before_tax_salary) - 3500 - 0.165 * int(before_tax_salary)
+        if take_tax < 0:
+            after_tax_salary = int(before_tax_salary) - 0.165 * int(before_tax_salary) 
+            print("{}:{:.2f}".format(id,after_tax_salary))
+        if 0<take_tax <= 1500:
 
-    elif 5000 < salary <= 8000:
-        tax = (salary - 3500) * 0.1 - 105
-        print('{:.2f}'.format(tax))
+             after_tax_salary = int(before_tax_salary) - 0.165 * int(before_tax_salary) - take_tax * 0.03
+             print("{}:{:.2f}".format(id,after_tax_salary))
+
+        if 1500 < take_tax <= 4500:
+        
+             after_tax_salary = int(before_tax_salary) - 0.165 * int(before_tax_salary) - take_tax * 0.1 + 105
+
+             print("{}:{:.2f}".format(id,after_tax_salary))
+        
+        if 4500 < take_tax <= 9000:
+        
+             after_tax_salary = int(before_tax_salary) - 0.165 * int(before_tax_salary) - take_tax * 0.2 + 555
+
+             print("{}:{:.2f}".format(id,after_tax_salary))
+
+        if 9000 < take_tax <= 35000:
+        
+             after_tax_salary = int(before_tax_salary) - 0.165 * int(before_tax_salary) - take_tax * 0.25 + 1005
+             print("{}:{:.2f}".format(id,after_tax_salary))
+
+        if 35000 < take_tax <= 55000:
+
+             after_tax_salary = int(before_tax_salary) - 0.165 * int(before_tax_salary) - take_tax * 0.3 + 2755
+
+             print("{}:{:.2f}".format(id,after_tax_salary))
+
+        if 55000 < take_tax <= 80000:
+
+             after_tax_salary = int(before_tax_salary) - 0.165 * int(before_tax_salary) - take_tax * 0.35 + 5505
+
+             print("{}:{:.2f}".format(id,after_tax_salary))
+
+
+        if 80000 < take_tax:
+             after_tax_salary = int(before_tax_salary) - 0.165 * int(before_tax_salary) - take_tax * 0.45 + 13505
+
+             print("{}:{:.2f}".format(id,after_tax_salary))
    
-    elif 8000 < salary <= 12500:
-        tax = (salary - 3500) * 0.2 - 555
-        print('{:.2f}'.format(tax))
-    elif 12500 < salary <= 38500:
-        tax = (salary - 3500) * 0.25 - 1005
-        print('{:.2f}'.format(tax))
-    elif 38500 < salary <= 58500:
-        tax = (salary - 3500) * 0.3 -2755
-        print("{:.2f}".format(tax))
-    elif 58500 < salary <= 83500:
-        tax = (salary - 3500) * 0.35 - 5505
-        print('{:.2f}'.format(tax))
-    elif 83500 < salary:
-        tax = (salary - 3500) * 0.45 -13505
-        print("{:.2f}".format(tax))
-    else:
-        raise ValueError()
+    
 except:
     print("Parameter Error")
 
